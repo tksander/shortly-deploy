7f97d51a12ca4b115b3820536104503a6b13c204
tksander <tkepasander@gmail.com>
2015-09-17 11:39:04 -0700

    Added ability to fetch files from db in mongoose.js

--- app/models/mongoose.js
+++ app/models/mongoose.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 var crypto = require('crypto');
 
-mongoose.connect('mongodb://localhost/test');
+mongoose.connect('mongodb://localhost/test/');
 
 var linkSchema = new Schema({
   url: String,
@@ -12,6 +12,10 @@ var linkSchema = new Schema({
   visits: Number
 });
 
+linkSchema.set('toObject', {getters: true}, {virtuals: false});
+
+
+ 
 var Link = mongoose.model('Link', linkSchema);
 
 var computeLinkCode = function(url) {
@@ -34,13 +38,61 @@ var modelMaker = function(url, title, base_url) {
 console.log(modelMaker("https://www.google.com", "Google", "http://shortlyondemandyo.azurewebsites.net/" ))
 
 
-var googleLink = new modelMaker("https://www.google.com", "Google", "http://shortlyondemandyo.azurewebsites.net/" )
+var googleLink = modelMaker("https://www.google.com", "Google", "http://shortlyondemandyo.azurewebsites.net/" )
+var amazonLink = modelMaker("https://www.amazon.com", "Amazon", "http://shortlyondemandyo.azurewebsites.net/" )
 
-googleLink.save(function (err) {
-  if(err) {
-    throw err;
-  }
-  console.log("success");
-});
+// amazonLink.save(function (err) {
+//   if(err) {
+//     throw err;
+//   }
+//   console.log("success");
+// });
+
+// googleLink.save(function (err) {
+//   if(err) {
+//     throw err;
+//   }
+//   console.log("success");
+// });
+
+ // { id: 1,
+ //       url: 'http://www.nytimes.com',
+ //       base_url: 'http://localhost:4568',
+ //       code: 'f606d',
+ //       title: 'The New York Times - Breaking News, World News & Multimedia',
+ //       visits: 2,
+ //       created_at: 1442425547876,
+ //       updated_at: 1442425562190 },
+
+var query = Link.find({})
+
+query.exec(function(err, array){
+  if(err) throw err
+
+
+  var backbone = []
+  array.forEach(function(item){
+    item  = item.toObject();
+    backbone.push({attributes:item});
+  })
+
+  console.dir(backbone);
+})
+
 
 exports.modelMaker = modelMaker;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
